3c64e07300af023622b06ac5c74dd90cbfb23439
Nihan Zyan <nihanzyan38@gmail.com>
2022-04-04 12:28:22 +0600

    Review JSON File Added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import image from '../images/travel.png'
+import './Home.css'
 
 const Home = () => {
     return (
-        <div>
-            <h1 className='text-3xl'>This is Home</h1>
+        <div className='travel-container flex items-center'>
+            <div className="travel-content text-left p-8">
+                <h1 className='text-4xl font-bold'>We Provide Top Destinations</h1>
+                <p className='text-1xl font-light'>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+            </div>
+            <div className="travel-img">
+                <img src={image} alt="" />
+            </div>
         </div>
     );
 };
